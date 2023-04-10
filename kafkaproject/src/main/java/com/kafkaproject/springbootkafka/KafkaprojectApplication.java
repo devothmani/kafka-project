38a392ef8554f4f4819d923147cb4704f71f2d60
EMEAAD\zothmani <zied.othmani@soprasteria.com>
2023-04-10 16:16:57 +0200

    init project kafka

---
+++ kafkaproject/src/main/java/com/kafkaproject/springbootkafka/KafkaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaproject.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaprojectApplication.class, args);
+	}
+
+}
